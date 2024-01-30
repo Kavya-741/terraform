3eb011f4f55caed98eb5975798ec9bdee2a942d9
Kavya-741 <118335053+Kavya-741@users.noreply.github.com>
2024-01-30 15:31:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ provider "aws" {
 region = "ap-south-a"
 }
 resource "aws_instance" "one" {
-ami = "ami-00952f27cf14db9cd"
-instance_type = "t2.micro"
-tags={
-name="Terraform Ec2"
-}
+ ami = "ami-00952f27cf14db9cd"
+ instance_type = "t2.micro"
+ tags={
+ name="Terraform Ec2"
+ }
 }
